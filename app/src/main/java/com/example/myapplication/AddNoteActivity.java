d60ced9a3c731dac3bab9b3dc358bbf3d6847505
student <student>
2019-07-15 20:02:23 +0300

    file work

--- app/src/main/java/com/example/myapplication/AddNoteActivity.java
+++ app/src/main/java/com/example/myapplication/AddNoteActivity.java
@@ -18,10 +18,24 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.myapplication.Database.DatabaseHelper;
 import com.example.myapplication.Database.Note;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static android.widget.Toast.makeText;
+import static com.facebook.stetho.inspector.network.PrettyPrinterDisplayType.JSON;
 
 public class AddNoteActivity extends AppCompatActivity {
     EditText etTitle;
@@ -32,6 +46,9 @@ public class AddNoteActivity extends AppCompatActivity {
     String title;
     String noteText;
     ImageView imgView;
+    private String NOTE_API_URL = "https://akirachixnotesapi.herokuapp.com/api/v1/notes";
+    private String TAG = "NOTES_API_RESPONSE";
+
 
     private static final int CAPTURE_IMAGE_REQUEST_CODE=500;
 
@@ -62,22 +79,24 @@ public class AddNoteActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                title=etTitle.getText().toString();
-                noteText=etNote.getText().toString();
-               Note notes=new Note(title,noteText);
-               DatabaseHelper databaseHelper=new DatabaseHelper(getBaseContext(),"notes",null,1);
-               long rows=databaseHelper.addNote(notes);
-                Log.d("AddNote","Number of notes is "+rows);
-//                Note note=new Note (title,noteText);
+                title = etTitle.getText().toString();
+                noteText = etNote.getText().toString();
+                PostNote(title, noteText);
+
+//               Note notes=new Note(title,noteText);
+//              DatabaseHelper databaseHelper=new DatabaseHelper(getBaseContext(),"notes",null,1);
+//               long rows=databaseHelper.addNote(notes);
+//                Log.d("AddNote","Number of notes is "+rows);
+////                Note note=new Note (title,noteText);
 
 //                Toast.makeText(getApplicationContext(),"you have clicked a save button",Toast.LENGTH_LONG).show();
 
 
             }
+
         });
 
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -89,6 +108,41 @@ public class AddNoteActivity extends AppCompatActivity {
 
         }
     }
+    public void PostNote(String title,String noteText){
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, NOTE_API_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG,response);
+                try {
+                JSONObject jsonObject=new JSONObject(response);
+                int id=jsonObject.getInt("id");
+                String title=jsonObject.getString("noteText");
+            }
+            catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG,error.getMessage());
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String>params=new HashMap<String, String>();
+                params.put("title","title");
+                params.put("noteText","noteText");
+                return  params;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
 }
+
+
 
 
--- app/src/main/java/com/example/myapplication/EditNoteActivity.java
+++ app/src/main/java/com/example/myapplication/EditNoteActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -15,9 +16,11 @@ import android.widget.ImageView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.myapplication.Database.DatabaseHelper;
 import com.example.myapplication.Database.Note;
 
@@ -25,14 +28,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EditNoteActivity extends AppCompatActivity {
-  private   int noteId;
-  private   EditText etTitle;
-  private   EditText etNote;
-  private   Button btnEditNote;
-  private   String title;
-  private   String noteText;
-  private   String NOTE_API_URL="https://akirachixnotesapi.herokuapp.com/api/v1/notes";
-  private String TAG="NOTES_API_RESPONSE";
+    private int noteId;
+    private EditText etTitle;
+    private EditText etNote;
+    private Button btnEditNote;
+    private String title;
+    private String noteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,32 +42,30 @@ public class EditNoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getNoteId();
-        etTitle=findViewById(R.id.etTitle);
-        etNote=findViewById(R.id.etNote);
+        etTitle = findViewById(R.id.etTitle);
+        etNote = findViewById(R.id.etNote);
         displayNote();
 
 
-        btnEditNote=findViewById(R.id.btnEditNote);
+        btnEditNote = findViewById(R.id.btnEditNote);
 
 
         btnEditNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                title=etTitle.getText().toString();
-                noteText=etNote.getText().toString();
-                Note notes=new Note(title,noteText);
-                DatabaseHelper databaseHelper=new DatabaseHelper(getBaseContext(),"notes",null,1);
-                long rows=databaseHelper.addNote(notes);
-                Log.d("AddNote","Number of notes is "+rows);
-                Note note=new Note (noteId,title,noteText);
+                title = etTitle.getText().toString();
+                noteText = etNote.getText().toString();
+                Note notes = new Note(title, noteText);
+                DatabaseHelper databaseHelper = new DatabaseHelper(getBaseContext(), "notes", null, 1);
+                long rows = databaseHelper.addNote(notes);
+                Log.d("AddNote", "Number of notes is " + rows);
+                Note note = new Note(noteId, title, noteText);
                 finish();
-
 
 
             }
         });
-
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -80,39 +79,17 @@ public class EditNoteActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void getNoteId(){
-        Bundle bundle=getIntent().getExtras();
-        if (bundle!=null){
-            noteId=bundle.getInt("Note_Id",0);
+    public void getNoteId() {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            noteId = bundle.getInt("Note_Id", 0);
         }
     }
-    public  void displayNote(){
-        DatabaseHelper databaseHelper=new DatabaseHelper(getApplicationContext(),"notes",null,1);
-        Note note=databaseHelper.getNoteById(noteId);
+
+    public void displayNote() {
+        DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext(), "notes", null, 1);
+        Note note = databaseHelper.getNoteById(noteId);
         etTitle.setText(note.getTitle());
         etNote.setText(note.getNoteText());
     }
-private  void  PostNote(String title,String noteText){
-    StringRequest stringRequest=new StringRequest(Request.Method.POST, NOTE_API_URL, new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            Log.d(TAG,response);
-
-        }
-    }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-Log.e(TAG,error.getMessage());
-        }
-    }){
-        @Override
-        protected Map<String, String> getParams() throws AuthFailureError {
-           Map<String,String>params=new HashMap<String, String>();
-           params.put("title","title");
-            params.put("noteText","noteText");
-            return  params;
-        }
-    };
-
-}
 }
